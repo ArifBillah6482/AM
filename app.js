3076038b26850aa9852380ad25087b7ee8753f68
Arif Billah <98398506+ArifBillah6482@users.noreply.github.com>
2022-02-22 11:16:55 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var Name;
+var num1 = prompt("First Name: ");
+var num2 = prompt("Last Name: ");
+Name =parseInt( Math.random() * 100);
+if(num1 == "Arif Billah" && num2 == "Mita"){
+    alert("Arif Billah loves Mita 100%.");
+}
+else{
+alert(num1 + " and " + num2 + " loves "+ Name + "%.");
+}
